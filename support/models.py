84b8f35eb6a0b3458aab45b2a5654111000046b4
Léo Da Rocha <leo.da.rocha2002@gmail.com>
2024-02-25 18:11:27 +0100

    create 'User' & 'clean' method

--- support/models.py
+++ support/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    birthdate = models.DateField()
+    can_be_contacted = models.BooleanField(default=False)
+    can_data_be_shared = models.BooleanField(default=False)
+
+    def clean(self):
+        super().clean()
+        today = timezone.now().date()
+        age = today.year - self.birthdate.year - ((today.month, today.day) < (self.birthdate.month, self.birthdate.day))
+        if age < 15:
+            raise ValidationError("User must be at least 15 years old")
+
+    def __str__(self):
+        return self.username
+
