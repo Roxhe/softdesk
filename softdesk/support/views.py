8fde3a7667ac2b0c1ce9fc91dd0ca31b113381bc
Léo DA ROCHA <leo.da.rocha2002@gmail.com>
2024-03-12 19:36:54 +0100

    add IsAuthenticated permission

--- softdesk/support/views.py
+++ softdesk/support/views.py
@@ -2,16 +2,19 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import User, Project, Issue, Comment, Contributor
 from .serializers import UserSerializer, ProjectSerializer, ContributorSerializer, IssueSerializer, CommentSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+    permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
         project = serializer.save(author=self.request.user)
@@ -21,13 +24,16 @@ class ProjectViewSet(viewsets.ModelViewSet):
 class ContributorViewSet(viewsets.ModelViewSet):
     queryset = Contributor.objects.all()
     serializer_class = ContributorSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class IssueViewSet(viewsets.ModelViewSet):
     queryset = Issue.objects.all()
     serializer_class = IssueSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated]
