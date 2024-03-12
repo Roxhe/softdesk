8b3fe4bb044561e5ce6f4f9b3af5e1fbc22da22e
Léo DA ROCHA <leo.da.rocha2002@gmail.com>
2024-03-12 19:14:00 +0100

    install and set up JWT urls

--- softdesk/softdesk/urls.py
+++ softdesk/softdesk/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from support.views import UserViewSet, ProjectViewSet, ContributorViewSet, IssueViewSet, CommentViewSet
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -14,5 +15,8 @@ router.register(r'comments', CommentViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('rest_framework.urls')),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include(router.urls)),
 ]
